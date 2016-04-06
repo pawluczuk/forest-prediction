d57d03d04e941e770642c4903422044f0fa922ea
pawluczuk <pawluczuk.monika@gmail.com>
2016-04-06 12:39:37 +0200

    basic training data statistics

--- forestPredictionPkg/R/hello.R
+++ forestPredictionPkg/R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -14,5 +14,12 @@
 #   Test Package:              'Cmd + Shift + T'
 
 hello <- function() {
-  print("Hello, world!")
+  print("Reading training data.")
+  training_data <- read.csv("data/train.csv")
+  # Statisticts
+  cat("\nNumber of training examples: ", length(training_data[,1]))
+  cat("\nNumber of variables: ", length(training_data))
+  cat("\nDistribution of forest cover types:")
+  print(table(training_data$Cover_Type))
+  hist(training_data$Cover_Type, main="Histogram of forest types in training set", xlab="Forest Cover type", xlim=c(0,7))
 }
