567784d7b7d1847112c6e2477cd72bf76ca58bd4
pawluczuk <pawluczuk.monika@gmail.com>
2016-04-25 22:27:43 +0200

    added standard analysis (min, max, std, etc) of attributes

--- forestPredictionPkg/R/hello.R
+++ forestPredictionPkg/R/hello.R
@@ -21,5 +21,42 @@ hello <- function() {
   cat("\nNumber of variables: ", length(training_data))
   cat("\nDistribution of forest cover types:")
   print(table(training_data$Cover_Type))
-  hist(training_data$Cover_Type, main="Histogram of forest types in training set", xlab="Forest Cover type", xlim=c(0,7))
-}
+  hist(training_data$Cover_Type, main="Histogram of forest types in training set", xlab="Forest Cover type", xlim=c(0,8))
+
+  names = c("Nazwa atrybutu", "Typ atrybutu", "Brakujące wartości", "Unikalne wartości", "Max", "Min", "Średnia", "Mediana", "Odchylenie standardowe")
+
+  mx <- matrix(NA, nrow = length(names(training_data)), ncol = length(names), byrow = TRUE)
+  counter <- 1
+  for (i in names(training_data)) {
+    print(i)
+    print(counter)
+    if (class(training_data[[paste(i)]]) == "integer") {
+
+      vector <- c(i,
+                  class(training_data[[paste(i)]]),
+                  sum(is.na(training_data[paste(i)])),
+                  length(unique(training_data[paste(i)])),
+                  max(training_data[paste(i)]),
+                  min(training_data[paste(i)]),
+                  mean(training_data[[paste(i)]]),
+                  median(training_data[[paste(i)]]),
+                  sd(training_data[[paste(i)]]))
+      mx[counter,] <- vector
+      counter <- counter+1
+
+    }
+    else {
+      vector <- c(i,
+                  class(training_data[[paste(i)]]),
+                  sum(is.na(training_data[paste(i)])),
+                  length(unique(training_data[paste(i)]))
+                )
+      mx[counter,] <- vector
+      counter <- counter+1
+    }
+
+  }
+  colnames(mx) <- names
+  print(mx)
+
+  }
